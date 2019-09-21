daebad448ca97978d08c1cc7f78c0277dedfc357
Junior <jnr-pires@hotmail.com>
2019-09-20 21:34:25 -0300

    added env config

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 module.exports = {
-  username: 'root',
-  password: 'root',
-  database: 'webfood',
-  host: '127.0.0.1',
+  username: process.env.DATABASE_USER || 'root',
+  password: process.env.DATABASE_PASSWORD || 'root',
+  database: process.env.DATABASE || 'webfood',
+  host: process.env.DATABASE_HOST || '127.0.0.1',
   dialect: 'mysql'
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const app = require('./app');
 
-app.listen(4000, () => console.log('Server running on port 4000'));
+app.listen( process.env.APP_PORT || 4000, () => console.log('Server running on port 4000'));
