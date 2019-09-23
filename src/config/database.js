06410f7b7fe153ba46150a9157edebda36ea66a6
Junior <jnr-pires@hotmail.com>
2019-09-23 12:23:43 -0300

    change database information

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 module.exports = {
   username: process.env.DATABASE_USER     || 'root',
   password: process.env.DATABASE_PASSWORD || 'root',
-  database: process.env.DATABASE          || 'webfood',
-  host    : process.env.DATABASE_HOST     || '127.0.0.1',
+  database: process.env.DATABASE_NAME     || 'webfood',
+  host    : process.env.DATABASE_HOST     || '0.0.0.0',
+  port    : '3306',
   dialect : 'mysql'
 };
