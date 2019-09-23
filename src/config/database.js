8cb12ab64f899262ed61e3c48bb3c19e3639b99d
Junior <jnr-pires@hotmail.com>
2019-09-23 10:33:17 -0300

    refactored some stuff

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-  username: process.env.DATABASE_USER || 'root',
+  username: process.env.DATABASE_USER     || 'root',
   password: process.env.DATABASE_PASSWORD || 'root',
-  database: process.env.DATABASE || 'webfood',
-  host: process.env.DATABASE_HOST || '127.0.0.1',
-  dialect: 'mysql'
+  database: process.env.DATABASE          || 'webfood',
+  host    : process.env.DATABASE_HOST     || '127.0.0.1',
+  dialect : 'mysql'
 };
