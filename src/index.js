9a72b35531c926badb6ece3aa90024ed7518437d
Junior <jnr-pires@hotmail.com>
2019-09-23 12:23:53 -0300

    fix dotenv

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const port = process.env.APP_PORT || 4000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(process.env.PORT || 4000, () => console.log('Server running ' + process.env.PORT || 4000));
