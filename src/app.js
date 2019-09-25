62a9294968c5e3659baa8da5111a274c12326930
Junior <jnr-pires@hotmail.com>
2019-09-25 14:14:08 -0300

    Changed of .findOne to .findByPk

--- src/app.js
+++ src/app.js
@@ -11,10 +11,11 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/category', routes.category);
-app.use('/subcategory', routes.subcategory);
-app.use('/product', routes.product);
-app.use('/employee', routes.employee);
-app.use('/client', routes.client);
+function loadRoutes(app, routes) {
+  for (let route in routes) 
+    app.use(`/api/${route}`, routes[route]);
+}
+
+loadRoutes(app, routes);
 
 module.exports = app;
