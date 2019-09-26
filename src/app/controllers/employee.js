488d7d72c4a8546f49afe4a2a137a23781cdfae0
Junior <jnr-pires@hotmail.com>
2019-09-26 16:13:38 -0300

    Added search by rg and name

--- src/app/controllers/employee.js
+++ src/app/controllers/employee.js
@@ -1,41 +1,86 @@
 var express = require('express');
+var Sequelize = require('sequelize');
 var router = express.Router();
 
 const { Employee } = require('../models');
 
-router.get('/', async (req, res) => {
-  const employee = await Employee.findAll(req.body);
-  res.json(employee);
-});
+const Op = Sequelize.Op;
 
-router.get('/enabled', async (req, res) => {
-  const employee = await Employee.findAll({ where: { enabled: 1 }});
-  res.json(employee);
+router.get('/', async (req, res) => {
+  await Employee.findAll({ where: { enabled: 1 }})
+    .then(employee => res.json(employee))
+    .catch(err => console.log("Error: " + err))
 });
 
 router.get('/disabled', async (req, res) => {
-  const employee = await Employee.findAll({ where: { enabled: 0 }});
-  res.json(employee);
+  await Employee.findAll({ where: { enabled: 0 }})
+    .then(employee => res.json(employee))
+    .catch(err => console.log("Error: " + err))
 });
 
-router.get('/:id', async (req, res) => {
-  const employee = await Employee.findOne({ where: { id: req.params.id }});
-  res.json(employee);
+router.get('/id/:id', async (req, res) => {
+  await Employee.findByPk(req.params.id)
+    .then(employee => res.json(employee))
+    .catch(err => console.log("Error: " + err))
+});
+
+router.get('/rg/:rg', async (req, res) => {
+  await Employee.findAll({
+    where: { 
+      rg: {
+        [Op.like]: `%${req.params.rg}%`
+      }
+    }
+  })
+    .then(employee => res.json(employee))
+    .catch(err => console.log("Error: " + err))
+});
+
+router.get('/name/:name', async (req, res) => {
+  await Employee.findAll({
+    where: { 
+      name: {
+        [Op.like]: `%${req.params.name}%`
+      }
+    }
+  })
+    .then(employee => res.json(employee))
+    .catch(err => console.log("Error: " + err))
 });
 
 router.post('/register', async (req, res) => {
-  const employee = await Employee.create(req.body);
-  res.json(employee);
+  await Employee.findOrCreate({
+    where: {
+      rg: req.body.rg
+    },
+    defaults: {
+      name: req.body.name,
+      phone: req.body.phone,
+      role: req.body.role,
+      permission: req.body.permission,
+      user: req.body.user,
+      password: req.body.password,
+      enabled: 1,
+    }
+  })
+    .then(([employee]) => {
+        res.json(employee.get({
+          plain: true,
+        }));
+    })
+    .catch(err => console.log("Error: " + err))
 });
 
 router.put('/update/:id', async (req, res) => {
-  const employee = await Employee.update(req.body, { where: { id: req.params.id }});
-  res.json(employee);
+  await Employee.update(req.body, { where: { id: req.params.id }})
+    .then(employee => res.json(employee))
+    .catch(err => console.log("Error: " + err))
 });
 
 router.delete('/delete/:id', async (req, res) => {
-  const employee = await Employee.destroy({ where: { id: req.params.id }});
-  res.json(employee);
+  await Employee.destroy({ where: { id: req.params.id }})
+    .then(employee => res.json(employee))
+    .catch(err => console.log("Error: " + err))
 });
 
 module.exports = router;
