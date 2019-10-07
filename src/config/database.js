b4877a377dda7e27cf456310934ede6accc294ee
Junior <jnr-pires@hotmail.com>
2019-10-06 23:17:27 -0300

    Changed database name

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   username: process.env.DB_USER     || 'root',
   password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME     || 'webfoodapi',
+  database: process.env.DB_NAME     || 'webfood',
   host    : process.env.DB_HOST     || '0.0.0.0',
   port    : '3306',
   dialect : 'mysql'
